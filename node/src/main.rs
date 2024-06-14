8ca34eb57a8cbb113a5a4cf81506e2ad34a7264e
QuantumFusion <memechi.kekamoto@pm.me>
2024-06-14 21:48:43 +0900

    initialized node with custom tokenomics

---
+++ node/src/main.rs
@@ -0,0 +1,13 @@
+//! Quantumfusion Node CLI library.
+#![warn(missing_docs)]
+
+mod benchmarking;
+mod chain_spec;
+mod cli;
+mod command;
+mod rpc;
+mod service;
+
+fn main() -> sc_cli::Result<()> {
+	command::run()
+}
